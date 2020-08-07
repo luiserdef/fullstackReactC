055025b2cf458b759a9ac9b6ee3856ed5819e3b5
Luis Sánchez <sanchezluis9820@gmail.com>
2020-08-06 21:04:20 -0500

    components part2

--- C2-FrontEndReact/cofusion/src/App.js
+++ C2-FrontEndReact/cofusion/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+    dishes:DISHES     
+    };
+  }
 
 
-function App() {
+render() {
   return (
     <div>
       <Navbar dark color="primary">
@@ -13,9 +22,10 @@ function App() {
           <NavbarBrand href="/">Ristorante  con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
