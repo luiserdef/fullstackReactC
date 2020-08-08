cc2bd97ca1b133332078417ea19791ee785e0632
Luis Sánchez <sanchezluis9820@gmail.com>
2020-08-07 20:22:41 -0500

    Single Page Applications Part 1

--- C2-FrontEndReact/cofusion/src/components/MainComponent.js
+++ C2-FrontEndReact/cofusion/src/components/MainComponent.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import Home from './HomeComponent'
 import Menu from './MenuComponent'
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import Contact from './ContactComponent'
 //import DishDetail from './DishdetailComponent'
 import Header from './HeaderComponent'
 import { DISHES } from '../shared/dishes';
@@ -11,26 +15,38 @@ class Main extends Component{
   constructor(props){
     super(props);
     this.state={
-    dishes:DISHES,   
+    dishes:DISHES,
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS   
     };
+
+        
   }
 render() {
-    const HomePage=()=>{
+    const HomePage = () => {
         return(
-            <Home/>
-        )
-    };
-  return (
+            <Home 
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}                
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
+        );
+      }
+
+  return (      
     <div>
       <Header/>
      <Switch>
          <Route path="/home" component={HomePage}/>
          <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
+         <Route exact path='/contactus' component={Contact}/>
          <Redirect to="/home"/>
      </Switch>
       <Footer/>
     </div>
   );
+  
 }
 }
 
