63d6934dd1a85ffcae399bfcdf641f7b2b962023
Luis Sánchez <sanchezluis9820@gmail.com>
2020-08-08 18:23:38 -0500

    react redux form

--- C2-FrontEndReact/cofusion/src/components/MainComponent.js
+++ C2-FrontEndReact/cofusion/src/components/MainComponent.js
@@ -18,9 +18,7 @@ const mapStateToProps = state => {
   }
 }
 class Main extends Component{
-constructor(props){
-  super(props);
-}
+
 render() {
       const DishWithId = ({match}) => {
         return(
