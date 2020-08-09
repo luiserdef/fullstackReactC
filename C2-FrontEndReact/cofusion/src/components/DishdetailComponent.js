8ec7bee03a9f88d79bb31548f326b03eded8d817
Luis Sánchez <sanchezluis9820@gmail.com>
2020-08-09 16:48:30 -0500

    redux thunk

--- C2-FrontEndReact/cofusion/src/components/DishdetailComponent.js
+++ C2-FrontEndReact/cofusion/src/components/DishdetailComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Row, Button, Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Container, Modal, ModalBody, ModalHeader, Label, FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form'
-
+import { Loading } from './LoadingComponent';
 
 class CommentForm extends Component {
     constructor(props) {
@@ -129,7 +129,25 @@ function RenderComments({comments, addComment, dishId}) {
     }
 }
 const DishDetail = (props) => {
-    
+                
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
     return (
         <Container>
             <div className="row">
