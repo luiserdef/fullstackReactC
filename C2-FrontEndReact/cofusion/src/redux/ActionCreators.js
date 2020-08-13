490db0039823b39f5d42ec4577d522be97f87fb2
Luis Sánchez <sanchezluis9820@gmail.com>
2020-08-13 03:16:01 -0500

    final project finished

--- C2-FrontEndReact/cofusion/src/redux/ActionCreators.js
+++ C2-FrontEndReact/cofusion/src/redux/ActionCreators.js
@@ -1,6 +1,5 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
-import { actions } from 'react-redux-form';
 
 export const addComment = (comment) => ({
     type: ActionTypes.ADD_COMMENT,
