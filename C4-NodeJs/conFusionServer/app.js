e09747890446f38bfd40e1a5dd0ff795724b8acb
Luis Sánchez <sanchezluis9820@gmail.com>
2020-08-17 01:18:44 -0500

    end express mongoose mongodb week 2

--- C4-NodeJs/conFusionServer/app.js
+++ C4-NodeJs/conFusionServer/app.js
@@ -10,6 +10,16 @@ var dishRouter=require('./routes/dishRouter');
 var promoRouter=require('./routes/promoRouter');
 var leaderRouter=require('./routes/leaderRouter');
 
+const mongoose= require('mongoose')
+const Dishes =require('./models/dishes');
+const url='mongodb://localhost:27017/conFusion'
+const connect=mongoose.connect(url)
+
+connect.then((db)=>{
+  console.log('Connected correctly to server')
+},(err)=>{console.log(err)})
+
+
 var app = express();
 
 // view engine setup
@@ -24,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dishRouter',dishRouter);
+app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
